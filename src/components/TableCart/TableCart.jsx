7ca36cda9de39b18dae2bb340fe85c5f28253f01
Gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-06-13 13:02:29 -0300

    add image to cart

--- src/components/TableCart/TableCart.jsx
+++ src/components/TableCart/TableCart.jsx
@@ -4,6 +4,9 @@ export default function TableCart({ item }) {
   return (
     <>
       <tr>
+        <Line>
+          <img src={item.thumbnail} alt="Item Carrinho" />
+        </Line>
         <Line>{item.title}</Line>
         <Line>${item.price}</Line>
         <Line>
